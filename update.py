df106e6c1cfdf391422c65b74d50175d2a7e073c
Sourcery AI <>
2022-08-02 19:11:53 +0000

    'Refactored by Sourcery'

--- update.py
+++ update.py
@@ -14,7 +14,7 @@ def main():
     with open('Dockerfile') as dockerfile:
         docker_from = dockerfile.readline()
 
-    image = re.search('^FROM (?P<image>.*)$', docker_from).group('image')
+    image = re.search('^FROM (?P<image>.*)$', docker_from)['image']
 
     result = subprocess.run(['docker', 'run', '-it', image, 'cat', '/usr/share/container-setup/ansible-test-ref.txt'],
                             check=True, capture_output=True, text=True)
@@ -38,19 +38,25 @@ def main():
                 content = [content]
 
             for item in content:
-                if label:
-                    if not item['name'].endswith('requirements.txt'):
-                        continue
-
-                    item['name'] = '%s.%s' % (label, item['name'])
-                elif item['name'] != 'constraints.txt':
+                if label and item['name'].endswith('requirements.txt'):
+                    item['name'] = f"{label}.{item['name']}"
+                elif (
+                    label
+                    and not item['name'].endswith('requirements.txt')
+                    or not label
+                    and item['name'] != 'constraints.txt'
+                ):
                     continue
 
                 files.append(item)
 
     requirements_dir = 'requirements'
 
-    untouched_paths = set(os.path.join(requirements_dir, file) for file in os.listdir(requirements_dir))
+    untouched_paths = {
+        os.path.join(requirements_dir, file)
+        for file in os.listdir(requirements_dir)
+    }
+
 
     for item in files:
         name = item['name']
@@ -71,23 +77,23 @@ def main():
             current_contents = ''
 
         if latest_contents == current_contents:
-            print('%s: current' % path)
+            print(f'{path}: current')
             continue
 
         with open(path, 'w') as contents_fd:
             contents_fd.write(latest_contents)
 
-        print('%s: updated' % path)
+        print(f'{path}: updated')
 
     for path in untouched_paths:
         os.unlink(path)
 
-        print('%s: deleted' % path)
+        print(f'{path}: deleted')
 
     # Error on any rename mappings that were not used to catch typos in the mapping or files that no longer exist
-    for url in untouched_mappings:
-        for m in untouched_mappings[url]:
-            print('ERROR: Unable to rename %s from %s' % (m, url))
+    for url, value in untouched_mappings.items():
+        for m in value:
+            print(f'ERROR: Unable to rename {m} from {url}')
 
     if any(untouched_mappings[url] for url in untouched_mappings):
         sys.exit(1)
